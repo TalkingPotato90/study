530cb46346728fa4db0475bc755d5f73fd52c077
potatomoong <star901210@hanmail.net>
2023-11-15 19:12:24 +0900

    Feat: 이벤트 미리보기 문구 출력 구현

--- src/main/java/christmas/View/OutputView.java
+++ src/main/java/christmas/View/OutputView.java
@@ -4,4 +4,10 @@ public class OutputView {
     public void welcome(){
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
+
+    public void showEventPreview(int visitDate) {
+        System.out.println("12월 " + visitDate + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+
+
 }
