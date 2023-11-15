55c9427bfc15208309d5cd4b780c297fd965faab
potatomoong <star901210@hanmail.net>
2023-11-15 21:12:45 +0900

    Fix: 메뉴 가격 수정

--- src/main/java/christmas/domain/MenuPrice.java
+++ src/main/java/christmas/domain/MenuPrice.java
@@ -8,18 +8,18 @@ public class MenuPrice {
 
     static {
         // 메뉴 가격 설정
-        MENU_PRICES.put("양송이수프", 5000);
-        MENU_PRICES.put("타파스", 8000);
-        MENU_PRICES.put("시저샐러드", 10000);
-        MENU_PRICES.put("티본스테이크", 30000);
-        MENU_PRICES.put("바비큐립", 25000);
-        MENU_PRICES.put("해산물파스타", 20000);
-        MENU_PRICES.put("크리스마스파스타", 22000);
-        MENU_PRICES.put("초코케이크", 12000);
-        MENU_PRICES.put("아이스크림", 7000);
+        MENU_PRICES.put("양송이수프", 6000);
+        MENU_PRICES.put("타파스", 5500);
+        MENU_PRICES.put("시저샐러드", 8000);
+        MENU_PRICES.put("티본스테이크", 55000);
+        MENU_PRICES.put("바비큐립", 54000);
+        MENU_PRICES.put("해산물파스타", 35000);
+        MENU_PRICES.put("크리스마스파스타", 25000);
+        MENU_PRICES.put("초코케이크", 15000);
+        MENU_PRICES.put("아이스크림", 5000);
         MENU_PRICES.put("제로콜라", 3000);
-        MENU_PRICES.put("레드와인", 25000);
-        MENU_PRICES.put("샴페인", 50000);
+        MENU_PRICES.put("레드와인", 60000);
+        MENU_PRICES.put("샴페인", 25000);
     }
 
     public static int getPrice(String menu) {
