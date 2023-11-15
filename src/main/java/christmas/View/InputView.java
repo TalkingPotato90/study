382a3608f1dff9e4f9a855d19fa8b964b66e0b6b
potatomoong <star901210@hanmail.net>
2023-11-15 15:25:56 +0900

    Feat: 예상 방문 날짜 유효성 검사 추가

--- src/main/java/christmas/View/InputView.java
+++ src/main/java/christmas/View/InputView.java
@@ -1,11 +1,22 @@
 package christmas.View;
 
 import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
-    public int inputDate(){
-        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
-        String date = Console.readLine();
-        int visitDate = Integer.parseInt(date);
-        return visitDate;
+    public int inputDate() {
+        while (true) {
+            try {
+                System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+                String date = Console.readLine();
+                int visitDate = Integer.parseInt(date);
+
+                if (1 <= visitDate && visitDate <= 31) {
+                    return visitDate;
+                }
+                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            } catch (NumberFormatException e) {
+                System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+            }
+        }
     }
 }
