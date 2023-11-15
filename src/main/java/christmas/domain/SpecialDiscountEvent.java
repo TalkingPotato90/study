71e5912019f120365f4e5c0502627316465d2acc
potatomoong <star901210@hanmail.net>
2023-11-15 23:02:26 +0900

    Test: 특별할인 테스트 구현

--- src/main/java/christmas/domain/SpecialDiscountEvent.java
+++ src/main/java/christmas/domain/SpecialDiscountEvent.java
@@ -10,7 +10,7 @@ public class SpecialDiscountEvent implements Event {
 
     private static final Set<LocalDate> SPECIAL_DATES = Set.of(
             LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 8), LocalDate.of(2023, 12, 15),
-            LocalDate.of(2023, 12, 22), LocalDate.of(2023, 12, 24), LocalDate.of(2023, 12, 25)
+            LocalDate.of(2023, 12, 22), LocalDate.of(2023, 12, 24), LocalDate.of(2023, 12, 25), LocalDate.of(2023, 12, 31)
     );
 
     @Override
@@ -20,9 +20,7 @@ public class SpecialDiscountEvent implements Event {
 
     @Override
     public int calculateDiscount(LocalDate visitDate, int totalOrderAmount) {
-        LocalDate currentDate = LocalDate.now();
-
-        if (totalOrderAmount >= 10000 && SPECIAL_DATES.contains(currentDate)) {
+        if (totalOrderAmount >= 10000 && SPECIAL_DATES.contains(visitDate)) {
             return SPECIAL_DISCOUNT_AMOUNT;
         }
 
