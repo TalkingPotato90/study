b6dbb867d514eee2721dea6af4145ac68d449bbe
potatomoong <star901210@hanmail.net>
2023-11-15 17:05:16 +0900

    Feat: 메뉴판에 없는 메뉴 입력 시 예외처리 구현

--- src/main/java/christmas/domain/OrderValidator.java
+++ src/main/java/christmas/domain/OrderValidator.java
@@ -1,6 +1,15 @@
 package christmas.domain;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class OrderValidator {
+    private static final Set<String> APPETIZER_MENU = new HashSet<>(Arrays.asList("양송이수프","타파스","시저샐러드"));
+    private static final Set<String> MAIN_MENU = new HashSet<>(Arrays.asList("티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타"));
+    private static final Set<String> DESSERT_MENU = new HashSet<>(Arrays.asList("초코케이크", "아이스크림"));
+    private static final Set<String> BEVERAGE_MENU = new HashSet<>(Arrays.asList("제로콜라", "레드와인", "샴페인"));
     public static boolean isValidOrder(String order){
         try {
             validateOnMenu(order);
@@ -17,7 +26,17 @@ public class OrderValidator {
     }
 
     private static void validateOnMenu(String order){
+        String[] orders = order.split(",");
 
+        for (String orderMenu : orders) {
+            String[] menuAndQuantity = orderMenu.split("-");
+            String menu = menuAndQuantity[0].trim();
+
+            if (!APPETIZER_MENU.contains(menu) && !MAIN_MENU.contains(menu) &&
+                    !DESSERT_MENU.contains(menu) && !BEVERAGE_MENU.contains(menu)) {
+                throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+            }
+        }
     }
 
     private static void validateQuantity(String order){
