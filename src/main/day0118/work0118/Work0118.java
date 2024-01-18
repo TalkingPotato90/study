c261c5e4eba8c7f0ce0e59321e8cdc72c254d25b
potatomoong <star901210@hanmail.net>
2024-01-18 22:22:56 +0900

    Refactor: 과제 코드 수정

--- src/main/day0118/work0118/Work0118.java
+++ src/main/day0118/work0118/Work0118.java
@@ -1,21 +1,30 @@
 package day0118.work0118;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class Work0118 {
+    public static final int MZ_LENGTH = 8;
+    public static final int PASSWORD_RANGE = 75;
+    public static final int PASSWORD_RANGE_CORRECTION = 48;
+    public static final int START_NUMBER_CODE_CORRECTION = 58;
+    public static final int START_UPPERCASE_CODE_CORRECTION = 64;
+    public static final int END_UPPERCASE_CODE_CORRECTION = 91;
+    public static final int START_LOWERCASE_CODE_CORRECTION = 96;
+    public static final int END_LOWERCASE_CODE_CORRECTION = 123;
+    public static final int LOTTO_RANGE = 45;
+    public static final int LOTTO_RANGE_CORRECTION = 1;
 
     public static void generateArray() {
         String[] mz = "어쩔티비,저쩔티비,뇌절티비,안물티비,안궁티비,때리고싶쥬~,못때리쥬~,개킹받쥬~".split(",");
-        int randIndex = (int) (Math.random() * 8);
+        int randIndex = (int) (Math.random() * MZ_LENGTH);
         System.out.println(mz[randIndex]);
     }
 
     public static char validateRandomNum() {
         char inRangeChar;
         while (true) {
-            char temp = (char) ((Math.random() * 75) + 48);
-            if ((temp < 58) || (temp > 64 && temp < 91) || (temp > 96 && temp < 123)) {
+            char temp = (char) ((Math.random() * PASSWORD_RANGE) + PASSWORD_RANGE_CORRECTION);
+            if ((temp < START_NUMBER_CODE_CORRECTION)
+                    || (temp > START_UPPERCASE_CODE_CORRECTION && temp < END_UPPERCASE_CODE_CORRECTION)
+                    || (temp > START_LOWERCASE_CODE_CORRECTION && temp < END_LOWERCASE_CODE_CORRECTION)) {
                 inRangeChar = temp;
                 break;
             }
@@ -31,16 +40,33 @@ public class Work0118 {
         System.out.println(password);
     }
 
-    public static int[] generateLottoNum() {
+    private static int generateRandomLottoNumber() {
+        return (int) ((Math.random() * LOTTO_RANGE) + LOTTO_RANGE_CORRECTION);
+    }
+
+    public static int[] generateLottoFormat() {
         int[] lottoNum = new int[6];
-        Set<Integer> uniqueNumbers = new HashSet<>();
-        for (int i = 0; i < lottoNum.length; ) {
-            int randomNumber = (int) ((Math.random() * 45) + 1);
-            if (uniqueNumbers.add(randomNumber)) {
-                lottoNum[i++] = randomNumber;
-            }
+        for (int i = 0; i < lottoNum.length; i++) {
+            lottoNum[i] = generateValidRandomNumber(lottoNum);
         }
         return lottoNum;
+    }
+
+    private static boolean checkNum(int[] lotto, int num) {
+        for (int value : lotto) {
+            if (value == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int generateValidRandomNumber(int[] randLotto) {
+        int randomNumber = generateRandomLottoNumber();
+        while (checkNum(randLotto, randomNumber)) {
+            randomNumber = generateRandomLottoNumber();
+        }
+        return randomNumber;
     }
 
     public static void printLotto(int[] array) {
@@ -52,8 +78,6 @@ public class Work0118 {
     public static void main(String[] args) {
         generateArray();
         generatePassword();
-        printLotto(generateLottoNum());
+        printLotto(generateLottoFormat());
     }
-
-
 }
