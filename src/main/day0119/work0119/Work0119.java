72c3335add3cf2e1ec89d65e7651c585b5bdaaa5
potatomoong <star901210@hanmail.net>
2024-01-20 00:11:37 +0900

    Feat: 과제 구현

--- src/main/day0119/work0119/Work0119.java
+++ src/main/day0119/work0119/Work0119.java
@@ -1,10 +1,22 @@
 package day0119.work0119;
 
+import day0119.TokenDataVO;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
 public class Work0119 {
 
-    public String retunBackUpFileName(String fileName){
+    public static final int KR = 0;
+    public static final int US = 1;
+    public static final int JP = 2;
+    public static final int CA = 3;
+
+    public String returnBackUpFileName(String fileName) {
 //        String result = "test_bak";
-        String result = fileName.concat("_bak");
+//        String result = fileName.concat("_bak");
 //        if(fileName.equals("temp")){
 //            result = "temp_bak";
 //        }
@@ -13,17 +25,86 @@ public class Work0119 {
 //        }else if(fileName.equals("temp.exe")){
 //            result = "temp_bak.exe";
 //        }
-        String tempFileName;
-        String tempExtensionName;
-        if(fileName.contains(".")){
-            tempFileName = fileName.substring(0, fileName.lastIndexOf("."));
-            tempExtensionName = fileName.substring(fileName.lastIndexOf("."));
-            result = tempFileName + "_bak" + tempExtensionName;
+
+//        String tempFileName;
+//        String tempExtensionName;
+//        if(fileName.contains(".")){
+//            tempFileName = fileName.substring(0, fileName.lastIndexOf("."));
+//            tempExtensionName = fileName.substring(fileName.lastIndexOf("."));
+//            result = tempFileName + "_bak" + tempExtensionName;
+//        }
+//
+//        return result;
+
+        StringBuilder result = new StringBuilder(fileName);
+
+        int lastDotIndex = fileName.lastIndexOf(".");
+        if (lastDotIndex != -1) {
+//            String tempFileName = fileName.substring(0, fileName.lastIndexOf("."));
+//            String tempExtensionName = fileName.substring(fileName.lastIndexOf("."));
+            String tempFileName = fileName.substring(0, lastDotIndex);
+            String tempExtensionName = fileName.substring(lastDotIndex);
+            result = new StringBuilder(tempFileName).append("_bak").append(tempExtensionName);
+        } else {
+            result.append("_bak");
         }
 
-        return result;
+        return result.toString();
     }
-    public static void main(String[] args) {
 
+    public String[] returnCsvData(String csvData) {
+        // 성이 아닌 이름에 '김'이 있으면 조짐
+        StringTokenizer stk = new StringTokenizer(csvData, ",.~ 김");
+        int tokenCnt = stk.countTokens();
+        String[] changeData = new String[tokenCnt];
+
+        int i = 0;
+        while (stk.hasMoreTokens()) {
+            changeData[i] = stk.nextToken();
+            i++;
+        }
+
+        return changeData;
+    }
+
+    public void printCsvData(String csvData) {
+        for (String value : returnCsvData(csvData)) {
+            System.out.println(value);
+        }
+    }
+
+    public String returnTime(int countryCode) {
+
+        if (countryCode < KR || countryCode > CA) {
+            countryCode = KR;
+        }
+        Locale result = Locale.KOREA;
+
+        if (countryCode == US) {
+            result = Locale.US;
+        } else if (countryCode == JP) {
+            result = Locale.JAPAN;
+        } else if (countryCode == CA) {
+            result = Locale.CANADA;
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss EEEE", result);
+
+        return sdf.format(new Date());
+    }
+
+    public static void main(String[] args) {
+        Work0119 work0119 = new Work0119();
+
+        System.out.println(work0119.returnBackUpFileName("test.txt"));
+        System.out.println(work0119.returnBackUpFileName("test"));
+
+        String csvData = "고한별,김도원,김동섭.김무영~김현종 박시현,손지민,김병년.김일신";
+        work0119.printCsvData(csvData);
+
+        System.out.println(work0119.returnTime(KR));
+        System.out.println(work0119.returnTime(US));
+        System.out.println(work0119.returnTime(JP));
+        System.out.println(work0119.returnTime(CA));
     }
 }
--- src/test/day0119/work0119/Work0119Test.java
+++ src/test/day0119/work0119/Work0119Test.java
@@ -1,8 +1,10 @@
 package day0119.work0119;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,44 +13,40 @@ class Work0119Test {
     @Test
     void 점_없는_파일명_반환() {
         Work0119 work0119 = new Work0119();
-        String result = work0119.retunBackUpFileName("test");
-        assertEquals("test_bak",result);
+        String result = work0119.returnBackUpFileName("test");
+        assertEquals("test_bak", result);
 
-        String result1 = work0119.retunBackUpFileName("temp");
-        assertEquals("temp_bak",result1);
+        String result1 = work0119.returnBackUpFileName("temp");
+        assertEquals("temp_bak", result1);
 
-        String result2 = work0119.retunBackUpFileName("what");
-        assertEquals("what_bak",result2);
+        String result2 = work0119.returnBackUpFileName("what");
+        assertEquals("what_bak", result2);
 
     }
 
     @Test
     void 점_있는_파일명_반환() {
         Work0119 work0119 = new Work0119();
-        String result = work0119.retunBackUpFileName("test.txt");
-        assertEquals("test_bak.txt",result);
+        String result = work0119.returnBackUpFileName("test.txt");
+        assertEquals("test_bak.txt", result);
 
-        String result1 = work0119.retunBackUpFileName("temp.exe");
-        assertEquals("temp_bak.exe",result1);
+        String result1 = work0119.returnBackUpFileName("temp.exe");
+        assertEquals("temp_bak.exe", result1);
 
-        String result2 = work0119.retunBackUpFileName("what.html");
-        assertEquals("what_bak.html",result2);
+        String result2 = work0119.returnBackUpFileName("what.html");
+        assertEquals("what_bak.html", result2);
     }
 
     @Test
-    void 매개변수_배열_반환() {
+    void 매개변수_배열_반환_김씨_제외() {
+
+        Work0119 work0119 = new Work0119();
+
+        String[] result = work0119.returnCsvData
+                ("김감자,김감각,정서중.메가도스~가습기 물병,위닉스,김호랑.김토끼");
+        assertArrayEquals(new String[]
+                {"감자", "감각", "정서중", "메가도스", "가습기", "물병", "위닉스", "호랑", "토끼"}, result);
+
     }
 
-    @Test
-    void 김씨_성_제외() {
-    }
-
-    @Test
-    void 날짜_반환() {
-
-    }
-
-    @Test
-    void 국가_확인() {
-    }
 }
