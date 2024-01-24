74903354a84839f591d28c1f4c6292f012d04347
potatomoong <star901210@hanmail.net>
2024-01-24 22:28:30 +0900

    Feat: 과제 해결

--- src/main/day0124/work0124/HomeWork.java
+++ src/main/day0124/work0124/HomeWork.java
@@ -1,47 +1,53 @@
 package day0124.work0124;
 
 import javax.swing.*;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class HomeWork {
     public static final int STUDENT_NUMBER = 0;
     public static final int STUDENT_NAME = 1;
     public static final int JAVA_SCORE = 2;
     public static final int ORACLE_SCORE = 3;
-    private Set<WorkVO> workSet;
+    private Set<Integer> workSet;
+    private Map<Integer, WorkVO> workMap;
     private boolean status = true;
 
     public HomeWork() {
         this.workSet = new HashSet<>();
+        this.workMap = new HashMap<>();
     }
 
     public void selectMode() {
         String inputData = JOptionPane.showInputDialog("메뉴\n1.입력 2.검색 3.종료");
-        if (inputData.equals("1")) {
-            convertInputData(inputData);
-        } else if (inputData.equals("2")) {
-            printFormat();
-        } else if (inputData.equals("3")) {
+        if(inputData == null){
             exitCondition();
+        } else if ("1".equals(inputData)) {
+            convertInputData(inputData);
+        } else if ("2".equals(inputData)) {
+            printFormat();
+        } else if ("3".equals(inputData)) {
+            exitCondition();
+        } else {
+            JOptionPane.showMessageDialog(null, "1에서 3 사이의 숫자만 입력하세요");
         }
     }
 
     public void convertInputData(String inputData) {
         inputData = JOptionPane.showInputDialog("학생의 정보를 아래와 같이 입력해주세요.\n예)학생번호,이름,자바점수,오라클점수");
 
-        if (inputData.startsWith("1")) {
+            String[] temp = inputData.split(",");
+            int number = Integer.parseInt(temp[STUDENT_NUMBER]);
+        if (workMap.containsKey(number)) {
             checkInputData();
             convertInputData(inputData);
         } else {
-            String[] temp = inputData.split(",");
-            int number = Integer.parseInt(temp[STUDENT_NUMBER]);
             String name = temp[STUDENT_NAME];
             int java = Integer.parseInt(temp[JAVA_SCORE]);
             int oracle = Integer.parseInt(temp[ORACLE_SCORE]);
             WorkVO workVO = new WorkVO(number, name, java, oracle);
 
-            workSet.add(workVO);
+            workMap.put(number, workVO);
+            workSet = workMap.keySet();
         }
     }
 
@@ -51,8 +57,12 @@ public class HomeWork {
 
     public void printFormat() {
         System.out.printf("%3s %5s %5s %5s %5s %5s %n", "번호", "이름", "자바", "오라클", "총점", "평균");
-        for (WorkVO workVO : workSet) {
-            workVO.printScore();
+        Iterator<Integer> keyValue = workSet.iterator();
+        int temp;
+        while (keyValue.hasNext()) {
+            temp = keyValue.next();
+            WorkVO value = workMap.get(temp);
+            value.printScore();
         }
     }
 
