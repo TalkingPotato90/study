8028b9bedfb360912e40572043caf057de17c93b
TalkingPotato90 <star901210@hanmail.net>
2024-03-20 22:40:42 +0900

    Feat: TDD 재시작

--- tdd-study/src/main/chap02/Calculator.java
+++ tdd-study/src/main/chap02/Calculator.java
@@ -1,7 +1,7 @@
 package chap02;
 
 public class Calculator {
-    public static int plus(int a1, int a2) {
-        return a1 + a2;
+    public static int plus(int a1, int a2){
+        return a1 + a2; // <- return 3; 코드를 수정함
     }
 }
--- tdd-study/src/test/chap02/CalculatorTest.java
+++ tdd-study/src/test/chap02/CalculatorTest.java
@@ -2,13 +2,13 @@ package chap02;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
     @Test
     void plus() {
         int result = Calculator.plus(1, 2);
         assertEquals(3, result);
-        assertEquals(5, Calculator.plus(4, 1));
+        assertEquals(5,Calculator.plus(4,1));
     }
 }
