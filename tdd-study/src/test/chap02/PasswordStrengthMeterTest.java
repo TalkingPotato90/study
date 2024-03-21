f5df83adcc69f4a8ffd87e29fe28528ccd01c1af
TalkingPotato90 <star901210@hanmail.net>
2024-03-21 22:14:21 +0900

    Feat: chapter02 진행

---
+++ tdd-study/src/test/chap02/PasswordStrengthMeterTest.java
@@ -0,0 +1,45 @@
+package chap02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PasswordStrengthMeterTest {
+    private PasswordStrengthMeter meter = new PasswordStrengthMeter();
+
+    private void assertStrength(String password, PasswordStrength expStr){
+        PasswordStrength result = meter.meter(password);
+        assertEquals(expStr, result);
+    }
+
+    @Test
+    void meetsAllCriteria_Then_Strong(){
+        // 코드 작성해야 함
+//        PasswordStrengthMeter meter = new PasswordStrengthMeter();
+//        값타입 결과 = meter.meter("ab12!@AB");
+//        assertEquals(기댓값, 결과);
+        assertStrength("ab12!@AB", PasswordStrength.STRONG);
+        assertStrength("abc1!Add", PasswordStrength.STRONG);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_Length_Then_Normal(){
+        assertStrength("ab12!@A", PasswordStrength.NORMAL);
+        assertStrength("Ab12!c", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void meetsOtherCriteria_except_for_number_Then_Normal(){
+        assertStrength("ab!@ABqwer", PasswordStrength.NORMAL);
+    }
+
+    @Test
+    void nullInput_Then_Invalid(){
+        assertStrength(null,PasswordStrength.INVALID);
+    }
+
+    @Test
+    void emptyInput_Then_Invalid(){
+        assertStrength("",PasswordStrength.INVALID);
+    }
+}
