8a2d6716ae565c3168f48f30c83cac34b16c780b
TalkingPotato90 <star901210@hanmail.net>
2024-03-22 23:33:32 +0900

    Feat: chapter02 진행

--- tdd-study/src/main/chap02/PasswordStrengthMeter.java
+++ tdd-study/src/main/chap02/PasswordStrengthMeter.java
@@ -1,24 +1,40 @@
 package chap02;
 
 public class PasswordStrengthMeter {
-    public PasswordStrength meter(String password){
-        if (password == null || password.isEmpty()){
+    public PasswordStrength meter(String password) {
+        if (password == null || password.isEmpty()) {
             return PasswordStrength.INVALID;
         }
 
-        if(password.length()<8) {
+        boolean lengthEnough = password.length() >= 8;
+        boolean containsNum = meetsContainingNumberCriteria(password);
+        boolean containsUpp = meetsContainingUppercaseCriteria(password);
+
+        if(lengthEnough && !containsNum && !containsUpp)
+            return PasswordStrength.WEAK;
+
+        if (!lengthEnough) {
             return PasswordStrength.NORMAL;
         }
+        if (!containsNum) return PasswordStrength.NORMAL;
+        if (!containsUpp) return PasswordStrength.NORMAL;
 
-        boolean containsNum = meetsContainingNumberCriteria(password);
 
-        if(!containsNum) return PasswordStrength.NORMAL;
         return PasswordStrength.STRONG;
     }
 
-    private boolean meetsContainingNumberCriteria(String password){
-        for (char ch : password.toCharArray()){
-            if (ch>='0' && ch<='9'){
+    private boolean meetsContainingNumberCriteria(String password) {
+        for (char ch : password.toCharArray()) {
+            if (ch >= '0' && ch <= '9') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean meetsContainingUppercaseCriteria(String password) {
+        for (char ch : password.toCharArray()) {
+            if (Character.isUpperCase(ch)) {
                 return true;
             }
         }
